04f97c32af388761871a58dd851a6675836710c7
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-09-09 16:08:15 +0100

    swtiched to boilerplate

--- wp-content/themes/herbalapothecary/functions.php
+++ wp-content/themes/herbalapothecary/functions.php
@@ -178,9 +178,3 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
-/**
- * Load WooCommerce compatibility file.
- */
-if ( class_exists( 'WooCommerce' ) ) {
-	require get_template_directory() . '/inc/woocommerce.php';
-}
