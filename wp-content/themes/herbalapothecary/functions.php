f0b5b7194d19ad208160a11673b5181d3b0fa1d9
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-09-29 15:30:32 +0100

    removed custom price function

--- wp-content/themes/herbalapothecary/functions.php
+++ wp-content/themes/herbalapothecary/functions.php
@@ -223,13 +223,6 @@ add_action("woocommerce_before_shop_loop", "shop_toolbar_start", 9);
 add_action("woocommerce_before_shop_loop", "shop_toolbar_end", 40);
 
 function price() {
-	global $post;
-
-	// Get the WC_Product_Variable instance Object
-	$product = wc_get_product( $post->ID ); // Works for any product type
-
-	// Displaying the formatted "Min" - "Max" price range
-	echo $product->get_price_html();
 }
 
 add_action("woocommerce_after_shop_loop_item_title", "price", 11);
