661026605c5aac7f252f7c0bd1238dadd196b07d
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-10-04 11:25:03 +0100

    added responsiveness to product page

--- wp-content/themes/herbalapothecary/woocommerce/content-single-product.php
+++ wp-content/themes/herbalapothecary/woocommerce/content-single-product.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying product content in the single-product.php template
  *
@@ -15,7 +16,7 @@
  * @version 3.6.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 global $product;
 
@@ -24,67 +25,65 @@ global $product;
  *
  * @hooked woocommerce_output_all_notices - 10
  */
-do_action( 'woocommerce_before_single_product' );
+do_action('woocommerce_before_single_product');
 
-if ( post_password_required() ) {
+if (post_password_required()) {
 	echo get_the_password_form(); // WPCS: XSS ok.
 	return;
 }
 ?>
-<div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
-	<div class="c-product-full">
+<div id="product-<?php the_ID(); ?>" <?php wc_product_class('c-product-full', $product); ?>>
+	<?php
+	/**
+	 * Hook: woocommerce_before_single_product_summary.
+	 *
+	 * @hooked woocommerce_show_product_sale_flash - 10
+	 * @hooked woocommerce_show_product_images - 20
+	 */
+	do_action('woocommerce_before_single_product_summary');
+	?>
+
+	<div class="summary entry-summary">
 		<?php
 		/**
-		 * Hook: woocommerce_before_single_product_summary.
+		 * Hook: woocommerce_single_product_summary.
 		 *
-		 * @hooked woocommerce_show_product_sale_flash - 10
-		 * @hooked woocommerce_show_product_images - 20
+		 * @hooked woocommerce_template_single_title - 5
+		 * @hooked woocommerce_template_single_rating - 10
+		 * @hooked woocommerce_template_single_price - 10
+		 * @hooked woocommerce_template_single_excerpt - 20
+		 * @hooked woocommerce_template_single_add_to_cart - 30
+		 * @hooked woocommerce_template_single_meta - 40
+		 * @hooked woocommerce_template_single_sharing - 50
+		 * @hooked WC_Structured_Data::generate_product_data() - 60
 		 */
-		do_action( 'woocommerce_before_single_product_summary' );
+		do_action('woocommerce_single_product_summary');
 		?>
-
-		<div class="summary entry-summary">
-			<?php
-			/**
-			 * Hook: woocommerce_single_product_summary.
-			 *
-			 * @hooked woocommerce_template_single_title - 5
-			 * @hooked woocommerce_template_single_rating - 10
-			 * @hooked woocommerce_template_single_price - 10
-			 * @hooked woocommerce_template_single_excerpt - 20
-			 * @hooked woocommerce_template_single_add_to_cart - 30
-			 * @hooked woocommerce_template_single_meta - 40
-			 * @hooked woocommerce_template_single_sharing - 50
-			 * @hooked WC_Structured_Data::generate_product_data() - 60
-			 */
-			do_action( 'woocommerce_single_product_summary' );
-			?>
+	</div>
+	<div class="c-product-full__sidebar">
+		<div class="c-product-full__sidebar-section">
+			<i class="fas fa-globe"></i>
+			<p>
+				We aim to dispatch products within 1 working day.
+			</p>
 		</div>
-		<div class="c-product-full__sidebar">
-			<div class="c-product-full__sidebar-section">
-				<i class="fas fa-globe"></i>
-				<p>
-					We aim to dispatch products within 1 working day.
-				</p>
-			</div>
-			<div class="c-product-full__sidebar-section">
-				<i class="fas fa-mobile-alt"></i>
-				<p>
-					Call us now for more info about our products on +44 (0)1947 602346
-				</p>
-			</div>
-			<div class="c-product-full__sidebar-section">
-				<i class="fas fa-pallet"></i>
-				<p>
-					Return unopened items within 14 days for a refund.
-				</p>
-			</div>
-			<div class="c-product-full__sidebar-section">
-				<i class="far fa-star"></i>
-				<p>
-					Review this product and let others know what you think.
-				</p>
-			</div>
+		<div class="c-product-full__sidebar-section">
+			<i class="fas fa-mobile-alt"></i>
+			<p>
+				Call us now for more info about our products on +44 (0)1947 602346
+			</p>
+		</div>
+		<div class="c-product-full__sidebar-section">
+			<i class="fas fa-pallet"></i>
+			<p>
+				Return unopened items within 14 days for a refund.
+			</p>
+		</div>
+		<div class="c-product-full__sidebar-section">
+			<i class="far fa-star"></i>
+			<p>
+				Review this product and let others know what you think.
+			</p>
 		</div>
 	</div>
 
@@ -96,8 +95,8 @@ if ( post_password_required() ) {
 	 * @hooked woocommerce_upsell_display - 15
 	 * @hooked woocommerce_output_related_products - 20
 	 */
-	do_action( 'woocommerce_after_single_product_summary' );
+	do_action('woocommerce_after_single_product_summary');
 	?>
 </div>
 
-<?php do_action( 'woocommerce_after_single_product' ); ?>
+<?php do_action('woocommerce_after_single_product'); ?>
