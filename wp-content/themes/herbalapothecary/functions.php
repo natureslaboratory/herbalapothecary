dbec869aa9abf2826758de9135ccd53eb1da42b1
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-10-06 14:43:09 +0100

    styled cart form

--- wp-content/themes/herbalapothecary/functions.php
+++ wp-content/themes/herbalapothecary/functions.php
@@ -294,4 +294,62 @@ function price()
 
 add_action("woocommerce_after_shop_loop_item_title", "price", 11);
 
+add_action( 'woocommerce_after_add_to_cart_quantity', 'ts_quantity_plus_sign' );
+ 
+function ts_quantity_plus_sign() {
+   echo '<button type="button" class="plus" >+</button>';
+   echo "</div>";
+}
+ 
+add_action( 'woocommerce_before_add_to_cart_quantity', 'ts_quantity_minus_sign' );
+function ts_quantity_minus_sign() {
+	echo "<div class='c-quantity'>";
+	echo '<button type="button" class="minus" >-</button>';
+}
+ 
+add_action( 'wp_footer', 'ts_quantity_plus_minus' );
+ 
+function ts_quantity_plus_minus() {
+   // To run this on the single product page
+   if ( ! is_product() ) return;
+   ?>
+   <script type="text/javascript">
+          
+      jQuery(document).ready(function($){   
+          
+            $('form.cart').on( 'click', 'button.plus, button.minus', function() {
+ 
+            // Get current quantity values
+            var qty = $( this ).closest( 'form.cart' ).find( '.qty' );
+            var val   = parseFloat(qty.val());
+            var max = parseFloat(qty.attr( 'max' ));
+            var min = parseFloat(qty.attr( 'min' ));
+            var step = parseFloat(qty.attr( 'step' ));
+ 
+            // Change the value if plus or minus
+            if ( $( this ).is( '.plus' ) ) {
+               if ( max && ( max <= val ) ) {
+                  qty.val( max );
+               } 
+            else {
+               qty.val( val + step );
+                 }
+            } 
+            else {
+               if ( min && ( min >= val ) ) {
+                  qty.val( min );
+               } 
+               else if ( val > 1 ) {
+                  qty.val( val - step );
+               }
+            }
+             
+         });
+          
+      });
+          
+   </script>
+   <?php
+}
+
 
