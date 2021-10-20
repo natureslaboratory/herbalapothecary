b0c436bc40d79a6658b99caabf03ef566a4e5001
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-10-20 17:33:15 +0100

    improved cart style

--- wp-content/themes/herbalapothecary/woocommerce/cart/cart.php
+++ wp-content/themes/herbalapothecary/woocommerce/cart/cart.php
@@ -153,8 +153,13 @@ do_action('woocommerce_before_cart'); ?>
 
 	<?php if (wc_coupons_enabled()) { ?>
 		<div class="coupon">
-			<label for="coupon_code"><?php esc_html_e('Coupon:', 'woocommerce'); ?></label> <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e('Coupon code', 'woocommerce'); ?>" /> <button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e('Apply coupon', 'woocommerce'); ?>"><?php esc_attr_e('Apply coupon', 'woocommerce'); ?></button>
-			<?php do_action('woocommerce_cart_coupon'); ?>
+			<h2>Coupon Discount</h2>
+			<label style="opacity: 0; position: absolute;" for="coupon_code"><?php esc_html_e('Coupon:', 'woocommerce'); ?></label> 
+			
+			<div class="coupon__wrapper">
+				<input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e('Coupon code', 'woocommerce'); ?>" /> <button type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e('Apply coupon', 'woocommerce'); ?>"><?php esc_attr_e('Apply coupon', 'woocommerce'); ?></button>
+				<?php do_action('woocommerce_cart_coupon'); ?>
+			</div>
 		</div>
 	<?php } ?>
 
