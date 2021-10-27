f47e4f8f484c493078715bbee8de29899680e2e8
Jack Barber <jack@natureslaboratory.co.uk>
2021-10-27 15:47:43 +0100

    Update functions.php

--- wp-content/themes/herbalapothecary/functions.php
+++ wp-content/themes/herbalapothecary/functions.php
@@ -620,3 +620,32 @@ function ha_edit_price_display($price)
 }
 
 add_filter('woocommerce_get_price_html', "ha_edit_price_display");
+
+function search_by_sku( $search, &$query_vars ) {
+    global $wpdb;
+    if(isset($query_vars->query['s']) && !empty($query_vars->query['s'])){
+        $args = array(
+            'posts_per_page'  => -1,
+            'post_type'       => 'product',
+            'meta_query' => array(
+                array(
+                    'key' => '_sku',
+                    'value' => $query_vars->query['s'],
+                    'compare' => 'LIKE'
+                )
+            )
+        );
+        $posts = get_posts($args);
+        if(empty($posts)) return $search;
+        $get_post_ids = array();
+        foreach($posts as $post){
+            $get_post_ids[] = $post->ID;
+        }
+        if(sizeof( $get_post_ids ) > 0 ) {
+                $search = str_replace( 'AND (((', "AND ((({$wpdb->posts}.ID IN (" . implode( ',', $get_post_ids ) . ")) OR (", $search);
+        }
+    }
+    return $search;
+    
+}
+add_filter( 'posts_search', 'search_by_sku', 999, 2 );
