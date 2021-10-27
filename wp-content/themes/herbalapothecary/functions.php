7705fb4cdaf42a8311ba833286d7985f58c89b51
Jack Barber <jack@natureslaboratory.co.uk>
2021-10-27 19:56:04 +0100

    Update functions.php

--- wp-content/themes/herbalapothecary/functions.php
+++ wp-content/themes/herbalapothecary/functions.php
@@ -643,10 +643,9 @@ function customizing_woocommerce_description( $content ) {
 	$info = get_post_meta($productId,$key,true);
 	$data = unserialize($info['product_descriptions'][0]);
 	foreach($data as $description){
-		if($description == 'tincture'){
-			$file = '/wp-content/themes/herbalapothecary/inc/'.$description.'.php';
-			$content .= file_get_contents($file);
-		}
+		$file = '/wp-content/themes/herbalapothecary/inc/'.$description.'.php';
+		$content .= $file;
+	    $content .= file_get_contents($file);
 	}
 
     return $content;
