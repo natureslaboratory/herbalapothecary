407cf9bd2426184d810144ad66e6de391436a903
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-10-28 11:45:17 +0100

    added debug line

--- wp-content/themes/herbalapothecary/functions.php
+++ wp-content/themes/herbalapothecary/functions.php
@@ -614,21 +614,26 @@ function ha_cron_exec_new()
 					}
 					
 				}
-			}
-			foreach ($variations as $variationArray) {
-				$variation_obj = new WC_Product_Variation($variationArray["variation_id"]);
-				$stock = $variation_obj->get_stock_quantity();
-				update_post_meta($variationArray["variation_id"], "_manage_stock", "yes");
-				// $stockStatus = get_post_meta($variation_obj->get_id(), "_stock_status");
-				// $debug["backorders"][] = [
-				// 	"id" => $variation_obj->get_id(),
-				// 	"stockStatus" => $stockStatus
-				// ];
-				// if (!in_array("instock", $stockStatus)) {
-				// 	update_post_meta($variation_obj->get_id(), "_stock_status", "outofstock");
-				// }
-				wc_update_product_stock($variationArray["variation_id"], $stock);
-				wc_delete_product_transients($variationArray["variation_id"]);
+			} else {
+				foreach ($variations as $variationArray) {
+					$variation_obj = new WC_Product_Variation($variationArray["variation_id"]);
+					$stock = $variation_obj->get_stock_quantity();
+					$debug["other_products"][] = [
+						"id" => $variationArray["variation_id"],
+						"stock" => $stock
+					];
+					update_post_meta($variationArray["variation_id"], "_manage_stock", "yes");
+					// $stockStatus = get_post_meta($variation_obj->get_id(), "_stock_status");
+					// $debug["backorders"][] = [
+					// 	"id" => $variation_obj->get_id(),
+					// 	"stockStatus" => $stockStatus
+					// ];
+					// if (!in_array("instock", $stockStatus)) {
+					// 	update_post_meta($variation_obj->get_id(), "_stock_status", "outofstock");
+					// }
+					wc_update_product_stock($variationArray["variation_id"], $stock);
+					wc_delete_product_transients($variationArray["variation_id"]);
+				}
 			}
 			// update_post_meta($variableProduct->get_id(), "_manage_stock", "yes");
 			// update_post_meta($variableProduct->get_id(), "_manage_stock", "no");
