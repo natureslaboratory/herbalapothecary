e213511eeffab5a63d03e973cf50838103075ee0
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-10-29 15:11:33 +0100

    improved counter code

--- wp-content/themes/herbalapothecary/scripts/search.js
+++ wp-content/themes/herbalapothecary/scripts/search.js
@@ -52,3 +52,29 @@ searchButtons.forEach(b => {
     console.log(b);
     b.setAttribute("aria-label", "Search");
 });
+
+
+const quantities = Array.from(document.getElementsByClassName("c-quantity"));
+
+quantities.forEach(q => {
+    const minus = q.getElementsByClassName("minus")[0];
+    const plus = q.getElementsByClassName("plus")[0];
+    const input = q.getElementsByClassName("input-text")[0];
+
+    function increment() {
+        let currentValue = parseInt(input.value);
+        if (currentValue < input.max) {
+            input.value = currentValue + 1;
+        }
+    }
+
+    function decrement() {
+        let currentValue = parseInt(input.value);
+        if (currentValue > input.min && currentValue > 1) {
+            input.value = parseInt(input.value) - 1;
+        }
+    }
+
+    minus.addEventListener("click", decrement);
+    plus.addEventListener("click", increment);
+})
