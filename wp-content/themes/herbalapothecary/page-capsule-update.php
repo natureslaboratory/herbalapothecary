6f9dc86f4647f16e03e3f9e3250ac872040ee596
acalebwilson24 <caleb@natureslaboratory.co.uk>
2022-02-21 11:01:43 +0000

    added new page layout for capsule update

---
+++ wp-content/themes/herbalapothecary/page-capsule-update.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package herbalapothecary
+ */
+
+get_header();
+
+// ha_cron_exec_new();
+
+set_capsule_stock();
+
+// ha_update_stock();
+
+get_footer();
