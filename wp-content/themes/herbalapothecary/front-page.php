f839777c00c7db03e1f02248a5f8270002d58084
Jack Barber <jack@natureslaboratory.co.uk>
2022-06-28 21:10:21 +0100

    Update front-page.php

--- wp-content/themes/herbalapothecary/front-page.php
+++ wp-content/themes/herbalapothecary/front-page.php
@@ -20,7 +20,7 @@ get_header();
                         We supply one of the largest ranges of herbal products in the UK including tinctures, fluid extracts, capsules &amp; creams as well as whole, cut and powdered herbs.<br /><br />
                         <small>Part of Nature's Laboratory, along with <a href="https://beevitalpropolis.com">BeeVital</a> and <a href="https://sweetcecilys.com">Sweet Cecily's</a></small>
                     </p>
-                    <a href="/about-us/" class="c-button">Shop Now</a>
+                    <a href="/shop/" class="c-button">Shop Now</a>
                 </div>
             </div>
         </div>
